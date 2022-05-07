89fc4f984e2b75a28481b2789ad3560edef3d748
Thomas Benjamin <105122168+Thomas-Benjamin@users.noreply.github.com>
2022-05-07 22:51:07 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Programa-2/main.c
@@ -0,0 +1,10 @@
+/* Esse é um programinha que cria uma árvore de natal */
+
+#include <stdio.h>
+int main(){
+  printf("      \033[0;33m*");
+  printf("\n      \033[0;32m*\n     ***\n    *****\n   ******* \n  ********* \n ***********");
+  printf("\033[0;31m \n\t | |");
+  printf("\033[0;36m \n\nThomas Benjamin ;)");
+  return 0;
+}
---
+++ .ccls-cache/@home@runner@Projeto2/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+  printf("      \033[0;33m*");
+  printf("\n      \033[0;32m*\n     ***\n    *****\n   ******* \n  ********* \n ***********");
+  printf("\033[0;31m \n\t | |");
+  printf("\033[0;36m \n\nThomas Benjamin ;)");
+  return 0;
+}
